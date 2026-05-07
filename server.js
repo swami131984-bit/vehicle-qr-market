30cc2690a7e015695ef4c74c99efec1216986673
swami131984-bit <swami131984@gmail.com>
2026-05-07 17:35:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Railway! Your QR app works.');
 });
 
+app.get('/health', (req, res) => res.send('ok'));
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
